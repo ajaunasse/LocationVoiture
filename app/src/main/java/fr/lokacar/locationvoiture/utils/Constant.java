6e14f352b043ddc50b8da3a61a8b07e2450c9293
Drapeau David <david4.dd@gmail.com>
2017-05-03 16:03:41 +0200

    Details Vehicule Activity

--- app/src/main/java/fr/lokacar/locationvoiture/utils/Constant.java
+++ app/src/main/java/fr/lokacar/locationvoiture/utils/Constant.java
@@ -8,5 +8,8 @@ public class Constant {
     //Pointe vers l'API symfony
     public static final String URL_LOGIN = "http://10.4.140.1:8080/ApiLocationVoiture/web/api/gerant/login?email=%s&password=%s";
     public static final String URL_VEHICULES = "http://10.4.140.1:8080/ApiLocationVoiture/web/api/vehicule/vehicules/%s";
+
+    //Clés utilisés dans la transmission des données d'une activité à l'autre
     public static final String INTENT_GERANT =  "INTENT_GERANT";
+    public static final String INTENT_VEHICULE = "INTENT_VEHICULE";
 }
