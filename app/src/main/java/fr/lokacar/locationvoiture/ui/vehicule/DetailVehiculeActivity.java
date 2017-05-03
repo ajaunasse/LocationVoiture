426eddf81a473860e90eb91b47c5835c0efd66bf
ajaunasse <alexandre.jaunasse@gmail.com>
2017-05-03 16:30:50 +0200

    Detail Vehicule

---
+++ app/src/main/java/fr/lokacar/locationvoiture/ui/vehicule/DetailVehiculeActivity.java
@@ -0,0 +1,15 @@
+package fr.lokacar.locationvoiture.ui.vehicule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.lokacar.locationvoiture.R;
+
+public class DetailVehiculeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_vehicule);
+    }
+}
