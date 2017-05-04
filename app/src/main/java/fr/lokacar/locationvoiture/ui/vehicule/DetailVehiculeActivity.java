a276ac4be435195e9d64e4ffb5ca8673c6a557d1
ajaunasse <alexandre.jaunasse@gmail.com>
2017-05-04 10:29:42 +0200

    Detail Vehicule

--- app/src/main/java/fr/lokacar/locationvoiture/ui/vehicule/DetailVehiculeActivity.java
+++ app/src/main/java/fr/lokacar/locationvoiture/ui/vehicule/DetailVehiculeActivity.java
@@ -2,12 +2,16 @@ package fr.lokacar.locationvoiture.ui.vehicule;
 
 import android.app.ActionBar;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import fr.lokacar.locationvoiture.R;
 import fr.lokacar.locationvoiture.model.Vehicule;
@@ -18,15 +22,27 @@ public class DetailVehiculeActivity extends AppCompatActivity {
 
     private ImageView imageViewVehicule;
     private AppBarLayout appBarLayout;
+    private TextView item_description;
+    private TextView item_prix;
+    private FloatingActionButton buttonRent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_vehicule);
         vehicule = (Vehicule) getIntent().getExtras().get(Constant.INTENT_VEHICULE);
         imageViewVehicule = (ImageView) findViewById(R.id.imageViewVehicule);
+        item_description = (TextView) findViewById(R.id.item_description);
+        item_prix = (TextView) findViewById(R.id.item_price);
+        buttonRent =(FloatingActionButton) findViewById(R.id.buttonRent) ;
+        if(vehicule.isEstLoue()) {
+            buttonRent.setEnabled(false);
+        }
+
         Picasso.with(DetailVehiculeActivity.this).load(vehicule.getImage()).into(imageViewVehicule);
-//        ActionBar ab = getActionBar();
-//        ab.setTitle(vehicule.getMarque() + vehicule.getLibelle());
+        setTitle(vehicule.getLibelle());
+        item_description.setText(vehicule.getCaracteristiques());
+        item_prix.setText(vehicule.getPrixString());
+
 
     }
 }
