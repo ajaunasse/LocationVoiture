41b35c459a636789fa96476668acb44d4e78236b
Drapeau David <david4.dd@gmail.com>
2017-05-04 09:15:21 +0200

    lol

--- app/src/main/java/fr/lokacar/locationvoiture/utils/Constant.java
+++ app/src/main/java/fr/lokacar/locationvoiture/utils/Constant.java
@@ -10,6 +10,8 @@ public class Constant {
     public static final String URL_VEHICULES = "http://10.4.140.1:8080/ApiLocationVoiture/web/api/vehicule/vehicules/%s";
     public static final String URL_CLIENTS = "http://10.4.140.1:8080/ApiLocationVoiture/web/api/client/clients";
 
+
+    
     //Clés utilisés dans la transmission des données d'une activité à l'autre
     public static final String INTENT_GERANT =  "INTENT_GERANT";
     public static final String INTENT_VEHICULE = "INTENT_VEHICULE";
